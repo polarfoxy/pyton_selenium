035e97ff52401bac67c861b4996939dab215c333
polarfoxy <esaprykina@ourgapps.com>
2018-07-23 18:54:02 +0300

    завершение вынесения параметров добавления теста для создания пустой группы

--- test_add_group.py
+++ test_add_group.py
@@ -14,12 +14,21 @@ class test_add_group(unittest.TestCase):
         self.wd = WebDriver()
         self.wd.implicitly_wait(60)
     
-    def test_test_add_group(self):
+    def test_add_group(self):
         wd = self.wd
         self.open_home_page(wd)
         self.login(wd, username="admin", password="secret")
         self.open_groups_page(wd)
-        self.create_group(wd)
+        self.create_group(wd, name="gfdhgf", header="fhgfh", footer="fhgfh")
+        self.return_to_groups_page(wd)
+        self.logout(wd)
+
+    def test_add_empty_group(self):
+        wd = self.wd
+        self.open_home_page(wd)
+        self.login(wd, username="admin", password="secret")
+        self.open_groups_page(wd)
+        self.create_group(wd, name="", header="", footer="")
         self.return_to_groups_page(wd)
         self.logout(wd)
 
@@ -29,7 +38,7 @@ class test_add_group(unittest.TestCase):
     def return_to_groups_page(self, wd):
         wd.find_element_by_link_text("group page").click()
 
-    def create_group(self, wd, name="gfdhgf", header ="fhgfh", footer="fhgfh"):
+    def create_group(self, wd, name, header, footer):
         # init group creation
         wd.find_element_by_name("new").click()
         # fill group form
