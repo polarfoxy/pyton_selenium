8bfc92afba489c2325389ee13e6b7a30e3e8d0f8
polarfoxy <esaprykina@ourgapps.com>
2018-07-25 15:41:37 +0300

    Добавлена группа для параметров создания контакта

--- contact.py
+++ contact.py
@@ -1,3 +1,27 @@
 
 
 class Contact:
+
+    def __init__(self, firstname, middlename, lastname, nickname, title, company, home, address, mobile, work,
+                       fax, email, email2, email3, homepage, byear, ayear, phone2, address2, notes):
+         self.firstname = firstname
+         self.middlename = middlename
+         self.lastname = lastname
+         self.nickname = nickname
+         self.title = title
+         self.company = company
+         self.home = home
+         self.address = address
+         self.mobile = mobile
+         self.work = work
+         self.fax = fax
+         self.email = email
+         self.email2 = email2
+         self.email3 = email3
+         self.homepage = homepage
+         self.byear = byear
+         self.ayear = ayear
+         self.phone2 = phone2
+         self.address2 = address2
+         self.notes = notes
+
