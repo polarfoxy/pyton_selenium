983e652c41823617b215d13f661a34008a7b5f15
polarfoxy <esaprykina@ourgapps.com>
2018-07-31 15:38:08 +0300

    выделен класс для управления сессиями

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -11,10 +11,10 @@ def app(request):
     return fixture
 
 def test_add_contact(app):
-    app.login(username="admin", password="secret")
+    app.session.login(username="admin", password="secret")
     app.create_contact(Contact(firstname="wtwetw", middlename="werwerwer", lastname="erwrwerewr", nickname="hhjhjjj",
                             title="hhjjkjj", company="jjjjjj", home="hhhhhh", address="hhhhh", mobile="hhhhhh",
                             work="hhhhhh", fax="hhhhh", email="hhhhh", email2="hhhhh", email3="hhhh", homepage="hhhhh",
                             byear="2000", ayear="2000", phone2="11111", address2="111111", notes="11111"))
-    app.logout()
+    app.session.logout()
 
