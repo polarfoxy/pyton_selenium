7a237e77bce1e2b281f624b7c046de21ae9af919
polarfoxy <esaprykina@ourgapps.com>
2018-07-31 15:50:52 +0300

    выделены классы для работы с группами и контактами

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -12,9 +12,9 @@ def app(request):
 
 def test_add_contact(app):
     app.session.login(username="admin", password="secret")
-    app.create_contact(Contact(firstname="wtwetw", middlename="werwerwer", lastname="erwrwerewr", nickname="hhjhjjj",
-                            title="hhjjkjj", company="jjjjjj", home="hhhhhh", address="hhhhh", mobile="hhhhhh",
-                            work="hhhhhh", fax="hhhhh", email="hhhhh", email2="hhhhh", email3="hhhh", homepage="hhhhh",
-                            byear="2000", ayear="2000", phone2="11111", address2="111111", notes="11111"))
+    app.contact.create(Contact(firstname="wtwetw", middlename="werwerwer", lastname="erwrwerewr", nickname="hhjhjjj",
+                               title="hhjjkjj", company="jjjjjj", home="hhhhhh", address="hhhhh", mobile="hhhhhh",
+                               work="hhhhhh", fax="hhhhh", email="hhhhh", email2="hhhhh", email3="hhhh", homepage="hhhhh",
+                               byear="2000", ayear="2000", phone2="11111", address2="111111", notes="11111"))
     app.session.logout()
 
