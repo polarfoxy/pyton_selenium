719b15e90f49b01448c3cf09d985ef6c144de09f
polarfoxy <esaprykina@ourgapps.com>
2018-08-03 16:04:59 +0300

    реализована проверка валидности фикстуры и проверка состояния сессии

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -4,10 +4,8 @@ from model.contact import Contact
 
 
 def test_add_contact(app):
-    app.session.login(username="admin", password="secret")
     app.contact.create(Contact(firstname="wtwetw", middlename="werwerwer", lastname="erwrwerewr", nickname="hhjhjjj",
                                title="hhjjkjj", company="jjjjjj", home="hhhhhh", address="hhhhh", mobile="hhhhhh",
                                work="hhhhhh", fax="hhhhh", email="hhhhh", email2="hhhhh", email3="hhhh", homepage="hhhhh",
                                byear="2000", ayear="2000", phone2="11111", address2="111111", notes="11111"))
-    app.session.logout()
 
--- test/test_mod_group.py
+++ test/test_mod_group.py
@@ -1,6 +1,10 @@
 from model.group import Group
 
 def test_mod_first_group(app):
-    app.session.login(username="admin", password="secret")
     app.group.mod_first_group(Group(name="123", header="123", footer="123"))
-    app.session.logout()
+
+def test_mod_first_group_name(app):
+    app.group.mod_first_group(Group(name="New group"))
+
+def test_mod_first_group_header(app):
+    app.group.mod_first_group(Group(header="New header"))
