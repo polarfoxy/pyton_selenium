dda92dbbb1c39eb4ba223a6c95964e93400a6b4b
polarfoxy <esaprykina@ourgapps.com>
2018-08-06 17:29:52 +0300

    Добавлены проверки перед переходом между страницами

--- fixture/application.py
+++ fixture/application.py
@@ -13,7 +13,9 @@ class Application:
 
     def open_home_page(self):
         wd = self.wd
-        wd.get("http://localhost/addressbook/")
+        if not wd.current_url.endswith("addressbook/"):
+            wd.get("http://localhost/addressbook/")
+
 
     def destroy(self):
         self.wd.quit()
